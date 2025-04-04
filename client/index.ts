5e67a29e406313841f18060422d1d649a6be23c3
Essential Randomness <essentialrandomn3ss@gmail.com>
2025-04-03 17:22:58 -0700

    get lexicon client to wokr

--- client/index.ts
+++ client/index.ts
@@ -1,4 +1,5 @@
 import { BskyAgent } from "@atproto/api";
+import { AtpBaseClient } from "./generated/api/index.js";
 
 const agent = new BskyAgent({ service: "https://bsky.social" });
 await agent.login({
@@ -7,3 +8,17 @@ await agent.login({
 });
 
 console.log(agent.did);
+
+const guestbookAgent = new AtpBaseClient(agent.fetchHandler);
+
+const createReult = await guestbookAgent.com.fujocoded.guestbook.book.create(
+  {
+    repo: "essentialrandom.bsky.social",
+    rkey: "emotional-support",
+  },
+  {
+    title: "Ms Boba's Emotional Support Guestbook",
+  }
+);
+
+console.dir(createReult, { depth: null });
