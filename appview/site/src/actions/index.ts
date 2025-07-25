a77b5f6b08a9d7dc149c76505257830e5befd326
Essential Randomness <essentialrandomn3ss@gmail.com>
2025-07-24 17:19:41 -0700

    allow creating a guestbook

--- appview/site/src/actions/index.ts
+++ appview/site/src/actions/index.ts
@@ -25,4 +25,25 @@ export const server = {
       return result.uri;
     },
   }),
+  createGuestbook: defineAction({
+    accept: "form",
+    input: z.object({
+      key: z.string().min(3).max(10),
+      title: z.string().max(200),
+    }),
+    handler: async (input, context) => {
+      const guestbookClient = context.locals.guestbookAgent;
+      const data = await guestbookClient.com.fujocoded.guestbook.book.create(
+        {
+          repo: context.locals.loggedInClient.did,
+          rkey: input.key,
+        },
+        {
+          title: input.title,
+        }
+      );
+
+      return data;
+    },
+  }),
 };
